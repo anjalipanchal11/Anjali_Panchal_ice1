2d610d36c004aea2dd110a78f740c7e3c962cf59
anjalipanchal11 <anjalipanchal115@gmail.com>
2020-01-29 19:26:32 -0500

    Changes

--- src/cardgameweek4/Card.java
+++ src/cardgameweek4/Card.java
@@ -2,21 +2,24 @@ package cardgameweek4;
 /**
  * Starter code for the Card class.
  * To be used in Week 4.
- * @author dancye, 2019
+ * @author Anjali Panchal, 2020
  */
 public class Card {
     
         public enum Suit {HEARTS, CLUBS,SPADES,DIAMONDS};
         public enum Value{ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING};
+        public enum Joker {JOKER};
         private final Suit suit;
         private final Value value;
+        //private final Joker joker;
         //change made in repository
         public Card(Suit s, Value gVal)
         {
            suit =s;
            value= gVal;
         }
-	public Value getValue() {
+        
+        public Value getValue() {
 		return this.value;
 	}
 
@@ -24,4 +27,5 @@ public class Card {
 	public Suit getSuit() {
 		return this.suit;
         }
+        
 }
